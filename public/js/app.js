c2706ce4aeaf49ba741e3fe83e4e1db14977efd3
Justin Dennison <justin1dennison@gmail.com>
2016-09-28 19:37:41 -0400

    implemented the ability to decrease seconds of the counter and wrap at 00 to 59

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,37 @@
 (function() {
   //initialize variables
   var startButton = $('#start');
-
+  var seconds = $('#seconds');
   // main functionality
   startButton.on('click', countdown);
 
   //function definitions
   function countdown(){
-      alert("It's the final countdown!!!");
+    var secondsText = seconds.text();
+    var secondsTextAsNumber = parseInt(secondsText);
+    // console.log(typeof secondsText);
+    // console.log(typeof secondsTextAsNumber);
+    if(secondsTextAsNumber === 0) {
+      seconds.text("59");
+      //then change seconds text to 59
+    } else {
+      var decreasedSecondsAsNumberByOne = secondsTextAsNumber - 1;
+      var padSecondsTextAsNumber = pad(decreasedSecondsAsNumberByOne);
+      seconds.text(padSecondsTextAsNumber); //this writes inside of the html
+    }
+
+    // var secondsValue = parseInt(seconds.text());
+    //
+    // seconds.text(pad(secondsValue - 1));
+  }
+
+  function pad(num){
+    if(num < 10){
+      //spit out the number with a leading zero
+      return "0" + num;
+    } else {
+      // spit out the original number
+      return num;
+    }
   }
 }());
