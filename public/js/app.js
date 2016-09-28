69259c6968bc9d1784bd94d30d559cc2e24c5430
Justin Dennison <justin1dennison@gmail.com>
2016-09-27 20:59:15 -0400

    initial setup of layout and start of javascript

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-var age = prompt('How old are you?');
+(function() {
+  //initialize variables
+  var startButton = $('#start');
+
+  // main functionality
+  startButton.on('click', countdown);
+
+  //function definitions
+  function countdown(){
+      alert("It's the final countdown!!!");
+  }
+}());
